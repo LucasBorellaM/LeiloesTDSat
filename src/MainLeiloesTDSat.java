04f3b26e3cfffb96b3e309cdaba8ab972a033a5a
Lucas Borella <lucasborella17@gmail.com>
2025-01-05 01:22:15 -0300

    Tela cadastroVIEW definida como tela principal

---
+++ src/MainLeiloesTDSat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lucas
+ */
+public class MainLeiloesTDSat {
+    public static void main(String[] args){
+        CadastroVIEW cadVIEW = new CadastroVIEW();
+        cadVIEW.setVisible(true);
+    }
+    
+}
